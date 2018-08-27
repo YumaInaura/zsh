261d5ad5fb22d49dc4794d222af3a864388e24c6
Yumainaura <yuma.inaura@gmail.com>
2018-08-27 19:36:40 +0900

    Fix comment install

--- install_widgets.sh
+++ install_widgets.sh
@@ -13,7 +13,7 @@ cat widgets/* | grep -A 1 '# Recipi'
 
 cat <<"MESSAGE"
 =============================
-~/.zshrc add below line
+~/.zshrc needs below line
 =============================
 for file in $(find ~/zle/widgets -type f); do source "$file"; done'
 echo 'source ~/zle/bindkey
